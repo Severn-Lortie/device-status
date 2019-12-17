ee9eca9da82ffcd38dd627403f9f2f8625043cf7
Severn Lortie <severn@Severns-MacBook-Pro.local>
2019-12-17 18:28:49 -0500

    added firebase SDK

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const app = express();
+const firebaseAdmin = require('firebase-admin')
 
+const app = express();
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
   extended: false
