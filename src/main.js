000534b95bd73820418aa5b3f992f44309b08bf7
Severn Lortie <severn@Severns-MacBook-Pro.local>
2019-12-17 23:02:42 -0500

    debug

--- src/main.js
+++ src/main.js
@@ -63,17 +63,16 @@ const getUpdatedDevices = async () => {
   // loop through them and find devices which have expired
   for (deviceKey in devices) {
     const lastPingDate = new Date(devices[deviceKey].lastPing); // turn the time string into a date object
-    console.log('comparision: ' + (Date.now() - lastPingDate.getTime()) > EXPIREY_TIME);
     if ((Date.now() - lastPingDate.getTime()) > EXPIREY_TIME) {
       
       // append a status and set it to false
       devices[deviceKey].status = false;
     } else {
       // otherwise, the device must be alive
+      console.log("applyig falsity")
       devices[deviceKey].status = true;
     }
   }
-  console.log(JSON.stringify(devices));
   return devices;
 }
 
