07e83a4efee06df3bdc21e65a87e344b33128b78
Severn Lortie <severn@Severns-MacBook-Pro.local>
2019-12-17 22:26:02 -0500

    used firebase for persistence

--- src/main.js
+++ src/main.js
@@ -1,36 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
 const firebaseAdmin = require('firebase-admin')
 
-// initalize firebase
-// check if the enviroment variable for auth is set
-let firebase;
-if (process.env.FIREBASE_CONFIG) {
-  // initalize the app with the contents of the enviroment var
-  admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_CONFIG)),
-    databaseURL: 'https://cta-display.firebaseio.com/'
-  });
-} else {
-  // otherwise load from a local file
-  admin.initializeApp({
-    credential: JSON.parse(require('./firebase-key.json')),
-    databaseURL: 'https://cta-display.firebaseio.com/'
-  });
-}
-
-
+/* initalize express */
 const app = express();
+
+// middlewear
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
   extended: false
 }));
 
-app.use(express.static('./public'));
 
-const getPingTimes = () => {
+/* initalize firebase */
+const EXPIREY_TIME = 1.8e+6;
 
+// local file with firebase creds
+let localCreds = require('./firebase-key.json');
+
+// check if the enviroment variable for auth is set
+if (process.env.FIREBASE_CONFIG) {
+  // initalize the app with the contents of the enviroment var
+  firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(JSON.parse(process.env.FIREBASE_CONFIG)),
+    databaseURL: 'https://cta-display.firebaseio.com/'
+  });
+} else {
+  // otherwise load from a local file
+  firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(localCreds),
+    databaseURL: 'https://cta-display.firebaseio.com/'
+  });
+}
+const database = firebaseAdmin.database();
+
+const getPingTime = () => {
   const now = new Date();
 
   // assemble last ping string
@@ -45,175 +49,69 @@ const getPingTimes = () => {
     timeZone: 'America/New_York'
   })
 
-  // milliseconds for ping time
-  const millis = now.getTime();
+  return timeString;
+}
 
-  return {
-    timeString,
-    millis
+const getUpdatedDevices = async () => {
+  const root = database.ref('/');
+  // TODO: error handeling
+
+  // get every device
+  const snapshot = await root.once('value');
+  const devices = snapshot.val();
+  
+  // loop through them and find devices which have expired
+  for (deviceKey in devices) {
+    const lastPingDate = new Date(devices[deviceKey].lastPing); // turn the time string into a date object
+    if ( (Date.now() - lastPingDate.getTime()) > EXPIREY_TIME ) {
+      // append a status and set it to false
+      devices[deviceKey].status = false;
+    } else {
+      // otherwise, the device must be alive
+      devices[deviceKey].status = true;
+    }
   }
+  return devices;
 }
 
-const makeOffline = (cb) => {
+app.use(express.static('src/public'));
 
-  const now = new Date();
-
-  // read the file
-  fs.readFile('./devices.json', (err, data) => {
-    const devices = JSON.parse(data);
-
-    for (key in devices) {
-      const device = devices[key];
-
-      // if the device has not pinged in 30 mins set status to inactive
-      if ((now.getTime() - device.lastPingMillis) > 1000 * 60 * 30) {
-        device.status = false;
+app.post('/devices/alive/:id', async (req, res) => {
+  // set the devices status and record its ping time
+  const root = database.ref('/');
+  try {
+    await root.update({
+      [req.params.id]: {
+        lastPing: getPingTime() // last ping time string
       }
-    }
-
-    // update the device statuses
-    fs.writeFile('./devices.json', JSON.stringify(devices), (err) => {
-      // call the callback with data or err
-      if (err) cb(err);
-      else cb();
     })
-  })
-}
-
-
-app.post('/devices/alive/:id', (req, res) => {
-
-  fs.readFile('./devices.json', (err, data) => {
-    if (err) res.status(500).json({
-      error: 'Unable to read device list'
+    res.json({message: "Device status updated"});
+  } catch (error) {
+    res.json({
+      // legacy support for announcement display means 'error' must be used
+      error: "Could not update status due to a database error: ${error.message}",
+      message: "Could not update status due to a database error: ${error.message}"
     });
-    else {
-
-      const devices = JSON.parse(data);
-
-      // check if the device exists
-      if (devices[req.params.id]) {
-        // add the new status
-        devices[req.params.id].status = req.body.status;
-
-        // add the ping time
-        const timings = getPingTimes();
-        devices[req.params.id].lastPing = timings.timeString;
-        devices[req.params.id].lastPingMillis = timings.millis;
-
-        // write the new file
-        fs.writeFile('./devices.json', JSON.stringify(devices), (err) => {
-          if (err) res.status(500).json({
-            error: 'Unable to update device status'
-          });
-          else {
-            res.status(200).json({
-              message: "Device status updated"
-            })
-          }
-        })
-      } else {
-        // error device not found
-        res.status(404).json({
-          error: 'Device not found'
-        })
-      }
-    }
-  })
+  }
 })
 
-app.post('/devices/:id', (req, res) => {
-
-  // load in JSON file
-  fs.readFile('./devices.json', (err, data) => {
-    if (err) res.status(500).json({
-      error: 'Unable to read device list'
-    });
-    else {
-
-      const devices = JSON.parse(data);
-
-      // check if device already exists
-      if (devices[req.params.id]) {
-        res.status(409).json({
-          error: "This device already exists"
-        })
-      } else {
-
-        // add the timings and status
-        const timings = getPingTimes();
-        devices[req.params.id] = {
-          status: true,
-          lastPing: timings.timeString,
-          lastPingMillis: timings.millis
-        };
-
-        // write the device
-        fs.writeFile('./devices.json', JSON.stringify(devices), (err) => {
-          if (err) res.status(500).json({
-            error: 'Unable to add device'
-          });
-          else {
-            res.status(200).json({
-              message: 'Device added'
-            })
-          }
-        })
-      }
-    }
-  })
+app.get('/devices', async (req, res) => {
+  try {
+    const devices = await getUpdatedDevices(); // get the updated list of devices
+    res.json(devices);
+  } catch(error) {
+    res.status(500).json({message: error.message});
+  }
 })
 
-app.get('/devices', (req, res) => {
-  makeOffline((err) => {
-    if (err) res.status(500).json({message: 'There was an issue updating device status'});
-    else {
-      fs.readFile('./devices.json', (err, data) => {
-        if (err) res.status(500).message({
-          error: 'Unable to read device list'
-        });
-        else {
-          res.status(200).json(JSON.parse(data));
-        }
-      })
-    }
-  })
-})
-
-app.delete('/devices/:id', (req, res) => {
-
-  // load in JSON file
-  fs.readFile('./devices.json', (err, data) => {
-    if (err) res.status(500).json({
-      error: 'Devices could not be loaded'
-    });
-    else {
-
-      const devices = JSON.parse(data);
-
-      // check if device exists
-      if (devices[req.params.id]) {
-        // delete the device
-        delete devices[req.params.id];
-
-        fs.writeFile('./devices.json', JSON.stringify(devices), (err) => {
-          if (err) res.status(500).json({
-            error: 'Unable to delete device'
-          })
-          else {
-            res.status(200).json({
-              message: 'Device deleted'
-            })
-          }
-        })
-
-      } else {
-        res.status(404).json({
-          error: 'Device does not exist'
-        })
-      }
-    }
-  })
+app.delete('/devices/:id', async (req, res) => {
+  const deviceRef = database.ref(`/${req.params.id}`);
+  try {
+   await deviceRef.remove();
+   res.json({message: 'Device has been deleted'})
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
 })
 
 app.listen(process.env.PORT || 8080)
